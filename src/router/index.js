beb2ae17ad89a3666b17174d2ca8def5b23a2f86
avishj <avish.j@protonmail.com>
2020-06-14 17:01:34 +0530

    Code Refactored to Vue.js Implementation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Profile from "../views/Profile.vue";
+import Timetable from "../views/Timetable.vue";
+import Verify from "../views/Verify.vue";
+import ResendVerify from "../views/ResendVerify.vue";
 
 Vue.use(VueRouter);
 
@@ -11,14 +18,40 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
+  },
+  {
+    path: "/timetable",
+    name: "Timetable",
+    component: Timetable
+  },
+  {
+    path: "/verify",
+    name: "Verify",
+    component: Verify
+  },
+  {
+    path: "/resend-verify",
+    name: "ResendVerify",
+    component: ResendVerify
+  },
 ];
 
 const router = new VueRouter({
